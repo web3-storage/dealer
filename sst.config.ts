6ec31b28907ed1dad6b3acb1a6a242fcb6f4e291
Vasco Santos <santos.vasco10@gmail.com>
2023-05-17 14:34:57 +0200

    feat: add api (#1)

---
+++ sst.config.ts
@@ -0,0 +1,32 @@
+import { Tags } from 'aws-cdk-lib';
+import { SSTConfig } from 'sst';
+import { ApiStack } from './stacks/ApiStack';
+
+export default {
+  config(_input) {
+    return {
+      name: 'spade-proxy',
+      region: 'us-west-2',
+    };
+  },
+  stacks(app) {
+    app.setDefaultFunctionProps({
+      runtime: 'nodejs18.x',
+      environment: {
+        NODE_OPTIONS: "--enable-source-maps",
+      },
+      nodejs: {
+        format: 'esm',
+        sourcemap: true
+      }
+    })
+    app.stack(ApiStack);
+
+    // tags let us discover all the aws resource costs incurred by this app
+    // see: https://docs.sst.dev/advanced/tagging-resources
+    Tags.of(app).add('Project', 'spade-proxy')
+    Tags.of(app).add('Repository', 'https://github.com/web3-storage/spade-proxy')
+    Tags.of(app).add('Environment', `${app.stage}`)
+    Tags.of(app).add('ManagedBy', 'SST')
+  }
+} satisfies SSTConfig;
---
+++ stacks/ApiStack.ts
@@ -0,0 +1,37 @@
+import {
+  Api,
+  StackContext,
+} from 'sst/constructs';
+
+import { getApiPackageJson, getGitInfo } from './config';
+
+export function ApiStack({ stack }: StackContext) {
+  // Setup API
+  const customDomain = {
+    domainName: undefined
+  }
+  // const customDomain = getCustomDomain(stack.stage, process.env.HOSTED_ZONE)
+  const pkg = getApiPackageJson()
+  const git = getGitInfo()
+
+  const api = new Api(stack, 'api', {
+    defaults: {
+      function: {
+        environment: {
+          NAME: pkg.name,
+          VERSION: pkg.version,
+          COMMIT: git.commmit,
+          STAGE: stack.stage,
+        }
+      }
+    },
+    routes: {
+      'GET /version': 'packages/functions/src/api/version.handler',
+    }
+  })
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+    CustomDomain: customDomain ? `https://${customDomain?.domainName}` : 'Set HOSTED_ZONE in env to deploy to a custom domain'
+  })
+}
